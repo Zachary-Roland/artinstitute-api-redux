96cd5da461bb11e096b4c67a729f170d51b0b269
Zachary-Roland <zsolo95@gmail.com>
2021-07-02 12:31:26 -0500

    adds router to app

--- src/App.js
+++ src/App.js
@@ -15,8 +15,62 @@ import Liked from "./components";
 import Login from "./components";
 import Search from "./components";
 
-function App() {
-  return <div></div>;
+function App({ username, clearLiked, clearSearch, clearUser }) {
+  return (
+    <Router>
+      <nav>
+        {/* If logged in, show search page. If not logged in, show log in page. */}
+        {!username && (
+          <NavLink activeClassName="active" to="/login">
+            Login
+          </NavLink>
+        )}
+        {username && (
+          <>
+            <NavLink activeClassName="active" to="/search">
+              Search
+            </NavLink>
+            <NavLink activeClassName="active" to="/liked">
+              Liked
+            </NavLink>
+            <NavLink
+              activeClassName="active"
+              to="/login"
+              onClick={() => {
+                clearFavorites();
+                clearSearch();
+                clearUser();
+              }}
+            >
+              Logout
+            </NavLink>
+          </>
+        )}
+      </nav>
+      <main>
+        <Switch>
+          <ProtectedRoute path="/login" reqUser={false} component={Login} />
+          <ProtectedRoute path="/search" reqUser={true} component={Search} />
+          <ProtectedRoute path="/liked" reqUser={true} component={Liked} />
+          <Route path="*">
+            <Redirect to="/login" />
+          </Route>
+        </Switch>
+      </main>
+    </Router>
+  );
 }
 
-export default App;
+function mapStateToProps(state) {
+  return {
+    username: state.username,
+  };
+}
+
+const mapDispatchToProps = {
+  clearLiked,
+  clearUser,
+  clearSearch,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
