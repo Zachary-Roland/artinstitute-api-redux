8f254442058c0c907d5d48efefa489746e98f25a
Zachary-Roland <zsolo95@gmail.com>
2021-07-02 12:01:16 -0500

    adds imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
 import "./App.css";
+import React from "react";
+import { connect } from "react-redux";
+import { clearLiked, clearSearch, clearUser } from "./redux/actions";
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+
+import Liked from "./components";
+import Login from "./components";
+import Search from "./components";
 
 function App() {
   return <div></div>;
