f91abc87aff83deb3102c9aa36d8c449e21ed1fa
Zachary-Roland <zsolo95@gmail.com>
2021-07-04 14:49:17 -0500

    adds data to results display w/ image

--- src/components/result.js
+++ src/components/result.js
@@ -1,16 +1,55 @@
 import React from "react";
-import { connect } from "react-redux";
+import {
+  Button,
+  Card,
+  CardContent,
+  Typography,
+  CardMedia,
+} from "@material-ui/core";
 
-const Result = () => {
+const Result = (result, setLiked, deleteLiked, isLiked, liked) => {
+  const artist = result.artist;
+  const id = result.id;
+  const title = result.title;
+  const view = result.view;
+  const altText = result.altText;
+  const img = result.img;
+  const date = result.date;
+  console.log(altText);
   return (
-    <>
-      <div>Result</div>
-    </>
+    <div>
+      <h3>
+        {title} by {artist} ({date})
+      </h3>
+      <img
+        src={`https://www.artic.edu/iiif/2/${img}/full/843,/0/default.jpg`}
+        alt={altText}
+        title={title}
+      ></img>
+      {view && <h6>(Currently on Display at AIC)</h6>}
+      {!view && <h6>(Not currently on Display at AIC)</h6>}
+      {isLiked && (
+        <button
+          onChange={() => {
+            // e.preventDefsault();
+            deleteLiked(id);
+          }}
+        >
+          Remove from Your Gallery
+        </button>
+      )}
+      {!isLiked && (
+        <button
+          onChange={() => {
+            setLiked({ artist, id, title, view, altText, img, date });
+            console.log(liked);
+          }}
+        >
+          Add to Your Gallery
+        </button>
+      )}
+    </div>
   );
 };
-function mapStateToProps(state) {
-  return {};
-}
-const mapDispatchToProps = {};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Result);
+export default Result;
