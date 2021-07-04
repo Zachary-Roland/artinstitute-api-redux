4cb95365633ce5ba1651811faa190f2f69b19608
Zachary-Roland <zsolo95@gmail.com>
2021-07-04 16:00:37 -0500

    adds material components

--- src/components/result.js
+++ src/components/result.js
@@ -2,11 +2,24 @@ import React from "react";
 import {
   Button,
   Card,
+  CardActions,
   CardContent,
   Typography,
   CardMedia,
 } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+import { classExpression } from "@babel/types";
 // import { addLiked } from "../redux/actions";
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 500,
+    margin: 50,
+  },
+  media: {
+    width: 500,
+  },
+});
 
 const Result = (result, addLiked, deleteLiked, isLiked, liked) => {
   const artist = result.artist;
@@ -16,20 +29,61 @@ const Result = (result, addLiked, deleteLiked, isLiked, liked) => {
   const altText = result.altText;
   const img = result.img;
   const date = result.date;
+  const classes = useStyles();
   console.log(altText);
   return (
-    <div>
-      <h3>
+    <Card className={classes.card}>
+      {/* <h3>
         {title} by {artist} ({date})
-      </h3>
-      <img
+      </h3> */}
+      <CardMedia
+        className={classes.media}
+        component="img"
+        src={`https://www.artic.edu/iiif/2/${img}/full/843,/0/default.jpg`}
+        title={title}
+        alt={altText}
+      />
+      <CardContent>
+        <Typography variant="h6" component="h2">
+          {title} by {artist} ({date})
+        </Typography>
+        {view && (
+          <Typography variant="body2" color="textSecondary" component="p">
+            Currently on Display at AIC
+          </Typography>
+        )}
+        {!view && (
+          <Typography variant="body2" color="textSecondary" component="p">
+            Not on Display at AIC
+          </Typography>
+        )}
+      </CardContent>
+      <CardActions>
+        {isLiked && (
+          <Button onClick={() => deleteLiked(id)}>
+            Remove from Your Gallery
+          </Button>
+        )}
+        {!isLiked && (
+          <Button
+            size="small"
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              console.log("button working");
+              addLiked({ artist, id, title, view, altText, img, date });
+            }}
+          >
+            Add to Your Gallery
+          </Button>
+        )}
+      </CardActions>
+      {/* <img
         src={`https://www.artic.edu/iiif/2/${img}/full/843,/0/default.jpg`}
         alt={altText}
         title={title}
-      ></img>
-      {view && <h6>(Currently on Display at AIC)</h6>}
-      {!view && <h6>(Not currently on Display at AIC)</h6>}
-      {isLiked && (
+      ></img> */}
+      {/* {isLiked && (
         <button onClick={() => deleteLiked(id)}>
           Remove from Your Gallery
         </button>
@@ -43,8 +97,8 @@ const Result = (result, addLiked, deleteLiked, isLiked, liked) => {
         >
           Add to Your Gallery
         </button>
-      )}
-    </div>
+      )} */}
+    </Card>
   );
 };
 
